a11c54546ceeb114717e677318599dacd70dee8e
Gustavo Máximo <gfmaximo97@gmail.com>
2021-01-04 16:01:08 -0300

    feat: Add fonts, expo AppLoading.

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
+const fetchFonts = () => {
+    Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+};
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setFontLoaded(true)}
+                onError={(err) => console.log(err)}
+            />
+        );
+    }
+
+    return (
+        <View>
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    );
 }
