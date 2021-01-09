dccc18298556625bf0fb5be512f1d339580e90fd
Gustavo Máximo <gfmaximo97@gmail.com>
2021-01-09 15:31:08 -0300

    feat: Add bottom tabs navigator.

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import Colors from '../constants/Colors';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import Categories from '../screens/Categories';
 import CategoryMeals from '../screens/CategoryMeals';
 import MealDetail from '../screens/MealDetail';
+import Favorites from '../screens/Favorites';
 
 const MealsNavigator = createStackNavigator(
     {
@@ -23,4 +28,42 @@ const MealsNavigator = createStackNavigator(
     }
 );
 
-export default createAppContainer(MealsNavigator);
+const restaurantIcon = tabInfo => (
+    <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+);
+
+const starIcon = tabInfo => <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: restaurantIcon,
+            tabBarColor: Colors.primaryColor,
+        },
+    },
+    Favorites: {
+        screen: Favorites,
+        navigationOptions: {
+            tabBarIcon: starIcon,
+            tabBarColor: Colors.accentColor,
+        },
+    },
+};
+
+const MealsFavoritesNavigator =
+    Platform.OS === 'android'
+        ? createMaterialBottomTabNavigator(tabScreenConfig, {
+              activeColor: 'white',
+              shifting: true,
+              barStyle: {
+                  backgroundColor: Colors.primaryColor,
+              },
+          })
+        : createBottomTabNavigator(tabScreenConfig, {
+              tabBarOptions: {
+                  activeTintColor: Colors.accentColor,
+              },
+          });
+
+export default createAppContainer(MealsFavoritesNavigator);
