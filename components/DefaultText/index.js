13e808d6a7a608f7d5b55d87de93de6f26ff62bb
Gustavo Máximo <gfmaximo97@gmail.com>
2021-01-17 23:42:25 -0300

    feat: Add dynamic text-align prop

--- components/DefaultText/index.js
+++ components/DefaultText/index.js
@@ -2,4 +2,5 @@ import styled from 'styled-components/native';
 
 export const DefaultText = styled.Text`
     font-family: open-sans;
+    text-align: ${({ align }) => (align ? align : 'auto')};
 `;
