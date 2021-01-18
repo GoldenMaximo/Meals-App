86933a81a6203b3b593b4c6ccca84085feafc1a1
Gustavo Máximo <gfmaximo97@gmail.com>
2021-01-17 23:43:47 -0300

    feat: Add no-favorites fallback on favorites screen.

--- screens/Favorites/index.js
+++ screens/Favorites/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-import { MealsList, CustomHeaderButton } from '../../components';
+import { MealsList, CustomHeaderButton, Screen, DefaultText } from '../../components';
 
 const Favorites = ({ navigation }) => {
-    const avaliableMeals = useSelector(state => state.meals.favoriteMeals);
+    const favoriteMeals = useSelector(state => state.meals.favoriteMeals);
 
-    return <MealsList mealData={avaliableMeals} navigation={navigation} />;
+    if (favoriteMeals && favoriteMeals.length) {
+        return <MealsList mealData={favoriteMeals} navigation={navigation} />;
+    }
+
+    return (
+        <Screen paddingHorizontal="40px">
+            <DefaultText align="center">
+                No favorite meals found. Use the meals navigator to add some!
+            </DefaultText>
+        </Screen>
+    );
 };
 
 Favorites.navigationOptions = ({ navigation }) => {
